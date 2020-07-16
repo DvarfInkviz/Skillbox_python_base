722e98ee28be92f76fa90ceeed41f17432e76de8
zlajsobaka@gmail.com <zlajsobaka@gmail.com>
2020-07-16 17:19:58 +0300

    4.02: user can choose color

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import simple_draw as sd
 
+sd.resolution = (1200, 1000)
 # Добавить цвет в функции рисования геом. фигур. из упр lesson_004/01_shapes.py
 # (код функций скопировать сюда и изменить)
 # Запросить у пользователя цвет фигуры посредством выбора из существующих:
@@ -14,6 +15,45 @@ import simple_draw as sd
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 
-# TODO здесь ваш код
+
+def shape_draw(_point, _length=100, _angle=0, _n_corner=3, _color=sd.COLOR_YELLOW):
+    prv_point = _point
+    for _i in range(_n_corner):
+        if _i == _n_corner-1:
+            sd.line(start_point=prv_point, end_point=_point, width=3, color=_color)
+        else:
+            vector = sd.get_vector(start_point=prv_point, angle=_angle + _i * 360 / _n_corner, length=_length, width=3)
+            prv_point = vector.end_point
+            vector.draw(color=_color)
+
+
+colors = {
+    1: sd.COLOR_RED,
+    2: sd.COLOR_ORANGE,
+    3: sd.COLOR_YELLOW,
+    4: sd.COLOR_GREEN,
+    5: sd.COLOR_CYAN,
+    6: sd.COLOR_BLUE,
+    7: sd.COLOR_PURPLE,
+    8: sd.random_color(),
+}
+
+while True:
+    user_input = input("Выберите цвет фигуры из следующего списка:\n1 : Красный\n2 : Оранжевый\n3 : Желтый\n4 : Зеленый"
+                       "\n5 : Голубой\n6 : Синий\n7 : Фиолетовый\n8 : случайный \nВаш выбор >>")
+    if user_input.isdigit():
+        color = int(user_input)
+        if 0 < color <= 8:
+            print('Вы ввели', color)
+            break
+        else:
+            print('Вы ввели некорректный номер цвета!')
+    else:
+        print('Вы ввели некорректный номер цвета!')
+
+# point = sd.get_point(300, 300)
+for i in range(3, 7):
+    point = sd.get_point(sd.random_number(100, 800), sd.random_number(100, 800))
+    shape_draw(_point=point, _length=200, _n_corner=i, _angle=0, _color=colors[color])
 
 sd.pause()
