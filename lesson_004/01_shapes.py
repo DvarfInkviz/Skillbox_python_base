dc876030a0d7016afafb42b71cfbf666336472e0
zlajsobaka@gmail.com <zlajsobaka@gmail.com>
2020-07-16 16:56:52 +0300

    4.01: shapes - 3 versions

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -2,6 +2,8 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 1000)
+
 # Часть 1.
 # Написать функции рисования равносторонних геометрических фигур:
 # - треугольника
@@ -36,7 +38,73 @@ import simple_draw as sd
 # sd.line()
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
-# TODO здесь ваш код
+
+def triangle(_point, _length=200, _angle=0):
+    v1 = sd.get_vector(start_point=_point, angle=_angle, length=_length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=_angle + 120, length=_length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=_angle + 240, length=_length, width=3)
+    v3.draw()
+
+
+def square(_point, _length=200, _angle=0):
+    v1 = sd.get_vector(start_point=_point, angle=_angle, length=_length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=_angle + 90, length=_length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=_angle + 180, length=_length, width=3)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=_angle + 270, length=_length, width=3)
+    v4.draw()
+
+
+def pentagon(_point, _length=200, _angle=0):
+    v1 = sd.get_vector(start_point=_point, angle=_angle, length=_length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=_angle + 72, length=_length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=_angle + 144, length=_length, width=3)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=_angle + 216, length=_length, width=3)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=_angle + 288, length=_length, width=3)
+    v5.draw()
+
+
+def hexagon(_point, _length=200, _angle=0):
+    v1 = sd.get_vector(start_point=_point, angle=_angle, length=_length, width=3)
+    v1.draw()
+
+    v2 = sd.get_vector(start_point=v1.end_point, angle=_angle + 60, length=_length, width=3)
+    v2.draw()
+
+    v3 = sd.get_vector(start_point=v2.end_point, angle=_angle + 120, length=_length, width=3)
+    v3.draw()
+
+    v4 = sd.get_vector(start_point=v3.end_point, angle=_angle + 180, length=_length, width=3)
+    v4.draw()
+
+    v5 = sd.get_vector(start_point=v4.end_point, angle=_angle + 240, length=_length, width=3)
+    v5.draw()
+
+    v6 = sd.get_vector(start_point=v5.end_point, angle=_angle + 300, length=_length, width=3)
+    v6.draw()
+
+
+triangle(_point=sd.random_point(), _angle=23, _length=100)
+square(_point=sd.random_point(), _angle=67, _length=100)
+pentagon(_point=sd.random_point(), _angle=35, _length=100)
+hexagon(_point=sd.random_point(), _angle=8, _length=100)
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
@@ -56,6 +124,75 @@ import simple_draw as sd
 # Не забудте в этой общей функции придумать, как устранить разрыв в начальной/конечной точках рисуемой фигуры
 # (если он есть. подсказка - на последней итерации можно использовать линию от первой точки)
 
+
+# TODO вариант номер 2 с общей функцией
+def vector_draw(_point, _angle, _length, _width, _color=sd.COLOR_YELLOW):
+    _vector = sd.get_vector(start_point=_point, angle=_angle, length=_length, width=_width)
+    _vector.draw(color=_color)
+    return _vector.end_point
+
+
+def triangle_v2(_point, _length=200, _angle=0):
+    _prv_point = _point
+    for _i in range(3):
+        if _i == 2:
+            sd.line(start_point=_prv_point, end_point=_point, width=3, color=sd.random_color())
+        else:
+            _prv_point = vector_draw(_point=_prv_point, _length=_length, _angle=_angle + _i * 360 / 3, _width=3,
+                                     _color=sd.random_color())
+
+
+def square_v2(_point, _length=200, _angle=0):
+    _prv_point = _point
+    for _i in range(4):
+        if _i == 3:
+            sd.line(start_point=_prv_point, end_point=_point, width=3, color=sd.random_color())
+        else:
+            _prv_point = vector_draw(_point=_prv_point, _length=_length, _angle=_angle + _i * 360 / 4, _width=3,
+                                     _color=sd.random_color())
+
+
+def pentagon_v2(_point, _length=200, _angle=0):
+    _prv_point = _point
+    for _i in range(5):
+        if _i == 4:
+            sd.line(start_point=_prv_point, end_point=_point, width=3, color=sd.random_color())
+        else:
+            _prv_point = vector_draw(_point=_prv_point, _length=_length, _angle=_angle + _i * 360 / 5, _width=3,
+                                     _color=sd.random_color())
+
+
+def hexagon_v2(_point, _length=200, _angle=0):
+    _prv_point = _point
+    for _i in range(6):
+        if _i == 5:
+            sd.line(start_point=_prv_point, end_point=_point, width=3, color=sd.random_color())
+        else:
+            _prv_point = vector_draw(_point=_prv_point, _length=_length, _angle=_angle + _i * 360 / 6, _width=3,
+                                     _color=sd.random_color())
+
+
+triangle_v2(_point=sd.random_point(), _angle=23, _length=100)
+square_v2(_point=sd.random_point(), _angle=67, _length=100)
+pentagon_v2(_point=sd.random_point(), _angle=35, _length=100)
+hexagon_v2(_point=sd.random_point(), _angle=8, _length=100)
+
+
+# TODO третий вариант с одной функцией, рисующей равносторонние n-угольники
+def shape_draw(_point, _length=100, _angle=0, _n_corner=3, _color=sd.COLOR_YELLOW):
+    prv_point = _point
+    for _i in range(_n_corner):
+        if _i == _n_corner-1:
+            sd.line(start_point=prv_point, end_point=_point, width=3, color=_color)
+        else:
+            vector = sd.get_vector(start_point=prv_point, angle=_angle + _i * 360 / _n_corner, length=_length, width=3)
+            prv_point = vector.end_point
+            vector.draw(color=_color)
+
+
+point = sd.get_point(900, 600)
+for i in range(3, 15):
+    shape_draw(_point=point, _length=200, _n_corner=i, _angle=(i-3)*15, _color=sd.random_color())
 # Часть 2-бис.
 # А теперь - сколько надо работы что бы внести изменения в код? Выгода на лицо :)
 # Поэтому среди программистов есть принцип D.R.Y. https://clck.ru/GEsA9
