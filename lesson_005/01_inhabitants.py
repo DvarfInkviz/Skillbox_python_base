ae1137746d94bb17ff599e91bdd0eeaf75c8b86b
Lyashko Alexander <zlajsobaka@gmail.com>
2020-07-20 10:45:16 +0300

    5.01: rooms residents

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,6 +3,9 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from room_1 import folks as residents_room_1
+from room_2 import folks as residents_room_2
 
-
+separator = ', '
+print(f'В комнате room_1 живут: {separator.join(residents_room_1)}')
+print(f'В комнате room_2 живут: {separator.join(residents_room_2)}')
