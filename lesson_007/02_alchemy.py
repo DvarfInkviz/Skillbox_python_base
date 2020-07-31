f88fc9fc674bfb696d63506d733506e69b982c0f
zlajsobaka@gmail.com <zlajsobaka@gmail.com>
2020-07-31 17:25:35 +0300

    7.02: alchemy v.01a

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # Создать прототип игры Алхимия: при соединении двух элементов получается новый.
-# Реализовать следующие элементы: Вода, Воздух, Огонь, Земля, Шторм, Пар, Грязь, Молния, Пыль, Лава.
+# Реализовать следующие элементы:
+# Вода, Воздух, Огонь, Земля, Шторм, Пар, Грязь, Молния, Пыль, Лава.
+# Water, Air, Fire, Earth, Storm, Steam, Dirt, Lightning, Dust, Lava
 # Каждый элемент организовать как отдельный класс.
 # Таблица преобразований:
 #   Вода + Воздух = Шторм
@@ -10,6 +12,58 @@
 #   Воздух + Огонь = Молния
 #   Воздух + Земля = Пыль
 #   Огонь + Земля = Лава
+class Water:
+    def __str__(self):
+        return 'Вода'
+
+    # def __add__(self, other):
+    #     self.content.extend(other.content)
+    #     return self
+
+
+class Air:
+    def __str__(self):
+        return 'Воздух'
+
+
+class Fire:
+    def __str__(self):
+        return 'Огонь'
+
+
+class Earth:
+    def __str__(self):
+        return 'Земля'
+
+
+class Storm:
+    def __str__(self):
+        return 'Шторм'
+
+
+class Steam:
+    def __str__(self):
+        return 'Пар'
+
+
+class Dirt:
+    def __str__(self):
+        return 'Грязь'
+
+
+class Lightning:
+    def __str__(self):
+        return 'Молния'
+
+
+class Dust:
+    def __str__(self):
+        return 'Пыль'
+
+
+class Lava:
+    def __str__(self):
+        return 'Лава'
 
 # Сложение элементов реализовывать через __add__
 # Если результат не определен - то возвращать None
@@ -19,7 +73,8 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+
+print(Water(), '+', Air(), '=')
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
