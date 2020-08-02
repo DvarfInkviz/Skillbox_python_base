a7dd219eb6dd25ba6744ba6cb53ae8b154a2b89f
zlajsobaka@gmail.com <zlajsobaka@gmail.com>
2020-08-02 15:23:50 +0300

    7.03: updates

--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -191,7 +191,7 @@ for citizen in citizens:
 day_x = randint(1, 50)
 my_cat = Cat()
 
-for day in range(1, 366):
+for day in range(1, 732):
     if day == day_x:
         for i, cat in enumerate(cats):
             citizens[i].take_cat(cat=cat, house=my_sweet_home, nickname='Васька_'+str(i))
@@ -217,7 +217,7 @@ for day in range(1, 366):
                 rips[cat.name] = day
     print(my_sweet_home)
     if len(rips) == len(citizens)+len(cats):
-        cprint('Все умерли... R.I.P.', color='red')
+        cprint('Все умерли на {} день. R.I.P.'.format(day), color='red')
         for item in rips.items():
             cprint('{} прожил {} дней'.format(item[0], item[1]), color='red')
         break
