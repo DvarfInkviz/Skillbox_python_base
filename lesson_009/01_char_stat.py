0deae52f71501bc21892d1f4ea94baebbd810bbc
Lyashko Alexander <zlajsobaka@gmail.com>
2020-08-11 12:14:33 +0300

    9.01 updates

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -38,13 +38,13 @@ class Statistic:
         self.sort_type = _sort_type
 
     def run(self):
-        # self.__init__(self.file_name, self.sort_order, self.sort_type)
         if self.file_name.endswith('.zip'):
             self.unzip()
         self.collect()
-        if self.sort_type == 'key':
-            self.refactor_stat()
-        print(self)
+        # if self.sort_type == 'value':
+        self.refactor_stat()
+        # print(self)
+        print('+---------+----------+')
 
     def unzip(self):
         _filename = []
@@ -58,9 +58,9 @@ class Statistic:
         with open(self.file_name, 'r', encoding='cp1251') as file:
             for i, line in enumerate(file):
                 self._collect_for_line(line=line[:-1])
-                # print(self.stat)
                 if i == 100:
                     break
+        print(self.stat.get)
 
     def _collect_for_line(self, line):
         for char in line:
@@ -72,27 +72,42 @@ class Statistic:
 
     def refactor_stat(self):
         self.sort_stat = {}
-        if self.sort_type == 'key':
-            self.sort_stat = {k: self.stat[k] for k in sorted(self.stat, key=self.stat.get, reverse=self.sort_order)}
+        _eng_rus = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
+                        'abcdefghijklmnopqrstuvwxyz' \
+                        'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' \
+                        'абвгдеёжзийклмнопрстуфхцчшщъьэюя'
+        self.sort_stat = {k: self.stat[k] for k in sorted(self.stat, key=lambda x: _eng_rus.index(x[0]))}
+        print(self.sort_stat)
+        # if self.sort_type == 'value':
+        #     self.sort_stat = {k: self.stat[k] for k in sorted(self.stat, key=self.stat.get, reverse=self.sort_order)}
+        # else:
+        #     self.sort_stat = sorted(self.stat, key=lambda x: _eng_rus.index(x))
 
     def __str__(self):
         _total = 0
-        if self.sort_type == 'key':
+        self.print_head()
+        if self.sort_type == 'value':
             for item in self.sort_stat.items():
-                print(f'|{item[0]:^7}|{item[1]:^10d}|')
+                print(f'|{item[0]:^9}|{item[1]:^10d}|')
                 _total += item[1]
-        elif not self.sort_order:
-            for item in reversed(sorted(self.stat.items())):
-                print(f'|{item[0]:^7}|{item[1]:^10d}|')
-                _total += item[1]
-        else:
-            for item in sorted(self.stat.items()):
-                print(f'|{item[0]:^7}|{item[1]:^10d}|')
-                _total += item[1]
-        return f'ИТОГО : {_total}'
+        # elif not self.sort_order:
+        #     for item in reversed(sorted(self.stat.items())):
+        #         print(f'|{item[0]:^9}|{item[1]:^10d}|')
+        #         _total += item[1]
+        # else:
+        #     for item in sorted(self.stat.items()):
+        #         print(f'|{item[0]:^9}|{item[1]:^10d}|')
+        #         _total += item[1]
+        print('+---------+----------+')
+        return f'|  ИТОГО  |{_total:^10d}|'
+
+    def print_head(self):
+        print('+---------+----------+')
+        print('|  буква  | частота  |')
+        print('+---------+----------+')
 
 
-new_stat = Statistic(_file_name='python_snippets\\voyna-i-mir.txt.zip', _sort_order=True, _sort_type='key')
+new_stat = Statistic(_file_name='python_snippets\\voyna-i-mir.txt.zip', _sort_order=True, _sort_type='value')
 text = ' по частоте по убыванию '
 print(f'{text:#^50}')
 new_stat.run()
@@ -101,16 +116,16 @@ new_stat.run()
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
 #  - по алфавиту по убыванию
-text = ' по частоте по возрастанию '
-print(f'{text:#^50}')
-new_stat.sort_order = False
-new_stat.run()
-text = ' по алфавиту по возрастанию '
-print(f'{text:#^50}')
-new_stat.sort_order = True
-new_stat.sort_type = 'value'
-new_stat.run()
-text = ' по алфавиту по убыванию '
-print(f'{text:#^50}')
-new_stat.sort_order = False
-new_stat.run()
+# text = ' по частоте по возрастанию '
+# print(f'{text:#^30}')
+# new_stat.sort_order = False
+# new_stat.run()
+# text = ' по алфавиту по возрастанию '
+# print(f'{text:#^30}')
+# new_stat.sort_order = False
+# new_stat.sort_type = 'key'
+# new_stat.run()
+# text = ' по алфавиту по убыванию '
+# print(f'{text:#^30}')
+# new_stat.sort_order = True
+# new_stat.run()
