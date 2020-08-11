85de8c7761038726f1adf2c2a0471c2d0601c7a4
zlajsobaka@gmail.com <zlajsobaka@gmail.com>
2020-08-11 22:23:04 +0300

    9.01 продолжаем работу...

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -44,7 +44,7 @@ class Statistic:
         # if self.sort_type == 'value':
         self.refactor_stat()
         # print(self)
-        print('+---------+----------+')
+        # print('+---------+----------+')
 
     def unzip(self):
         _filename = []
@@ -60,7 +60,6 @@ class Statistic:
                 self._collect_for_line(line=line[:-1])
                 if i == 100:
                     break
-        print(self.stat.get)
 
     def _collect_for_line(self, line):
         for char in line:
@@ -72,24 +71,30 @@ class Statistic:
 
     def refactor_stat(self):
         self.sort_stat = {}
+        # for k in self.stat:
+        #     print(k, end=' ')
+        for k in sorted(self.stat):
+            print(k, end=' ')
         _eng_rus = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' \
-                        'abcdefghijklmnopqrstuvwxyz' \
-                        'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' \
-                        'абвгдеёжзийклмнопрстуфхцчшщъьэюя'
-        self.sort_stat = {k: self.stat[k] for k in sorted(self.stat, key=lambda x: _eng_rus.index(x[0]))}
+                   'abcdefghijklmnopqrstuvwxyz' \
+                   'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' \
+                   'абвгдеёжзийклмнопрстуфхцчшщъьэюя'
+        self.sort_stat = {_eng_rus[k]: self.stat[_eng_rus[k]] for k in sorted(self.stat, key=lambda x: _eng_rus.index(x))}
         print(self.sort_stat)
         # if self.sort_type == 'value':
-        #     self.sort_stat = {k: self.stat[k] for k in sorted(self.stat, key=self.stat.get, reverse=self.sort_order)}
+        self.sort_stat = {k: self.stat[k] for k in sorted(self.stat, key=self.stat.__getitem__, reverse=False)}
+        print()
+        print(self.sort_stat)
         # else:
         #     self.sort_stat = sorted(self.stat, key=lambda x: _eng_rus.index(x))
 
     def __str__(self):
         _total = 0
-        self.print_head()
-        if self.sort_type == 'value':
-            for item in self.sort_stat.items():
-                print(f'|{item[0]:^9}|{item[1]:^10d}|')
-                _total += item[1]
+        print('+---------+----------+\n|  буква  | частота  |\n+---------+----------+')
+        # if self.sort_type == 'value':
+        #     for item in self.sort_stat.items():
+        #         print(f'|{item[0]:^9}|{item[1]:^10d}|')
+        #         _total += item[1]
         # elif not self.sort_order:
         #     for item in reversed(sorted(self.stat.items())):
         #         print(f'|{item[0]:^9}|{item[1]:^10d}|')
@@ -100,11 +105,6 @@ class Statistic:
         #         _total += item[1]
         print('+---------+----------+')
         return f'|  ИТОГО  |{_total:^10d}|'
-
-    def print_head(self):
-        print('+---------+----------+')
-        print('|  буква  | частота  |')
-        print('+---------+----------+')
 
 
 new_stat = Statistic(_file_name='python_snippets\\voyna-i-mir.txt.zip', _sort_order=True, _sort_type='value')
