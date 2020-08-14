c590a5cb70f14ea097f57d2f8bd5bdce25365549
zlajsobaka@gmail.com <zlajsobaka@gmail.com>
2020-08-14 21:53:12 +0300

    10.1 try - except

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -5,9 +5,17 @@
 BRUCE_WILLIS = 42
 
 input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f'- Leeloo Dallas! Multi-pass № {result}!')
+try:
+    leeloo = int(input_data[4])
+except ValueError:
+    print('невозможно преобразовать к числу: ')
+except IndexError:
+    print('выход за границы списка: ')
+except Exception as exc:
+    print('Неожиданная ошибка:', exc)
+else:
+    result = BRUCE_WILLIS * leeloo
+    print(f'- Leeloo Dallas! Multi-pass № {result}!')
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
