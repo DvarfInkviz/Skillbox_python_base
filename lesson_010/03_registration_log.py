935d6f5edbb4cab2f39e4d2b2174f2766de76a10
zlajsobaka@gmail.com <zlajsobaka@gmail.com>
2020-08-15 09:46:52 +0300

    10.3 parse log

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,64 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+def _write_log(_message, _file):
+    with open(_file, 'a', encoding='UTF8') as file:
+        file.write(f'{_message}\n')
+
+
+def _parse_line(_line):
+    _data = _line.split(' ')
+    if len(_line) == 0 or _line.isspace():
+        raise ValueError('НЕ присутсвуют все три поля!')
+    elif len(_data) == 3:
+        if not _data[0].isalpha():
+            raise NotNameError('поле имени содержит НЕ только буквы!')
+        if '@' not in _data[1] or '.' not in _data[1]:
+            raise NotEmailError('поле емейл НЕ содержит @ и/или .(точку)')
+        if _data[2].isalnum():
+            if not 10 <= int(_data[2]) <= 99:
+                raise ValueError('поле возраст НЕ является числом от 10 до 99!')
+        else:
+            raise ValueError('поле возраст НЕ является числом!')
+    else:
+        raise IndexError('Не все поля заполнены!')
+
+
+class LogParser:
+
+    def __init__(self, _log_file):
+        self.log_file = _log_file
+        self.good_log = 'registrations_good.log'
+        self.bad_log = 'registrations_bad.log'
+
+    def run(self):
+        self.parse_file()
+        print('All Done!')
+
+    def parse_file(self):
+        with open(self.log_file, 'r', encoding='UTF8') as file:
+            for line in file:
+                try:
+                    _parse_line(_line=line[:-1])
+                except ValueError as exc:
+                    _write_log(_message=f'{line[:-1]:<50} - ERROR: {exc.args[0]}', _file=self.bad_log)
+                except NotNameError as exc:
+                    _write_log(_message=f'{line[:-1]:<50} - ERROR: {exc.args[0]}', _file=self.bad_log)
+                except NotEmailError as exc:
+                    _write_log(_message=f'{line[:-1]:<50} - ERROR: {exc.args[0]}', _file=self.bad_log)
+                except IndexError as exc:
+                    _write_log(_message=f'{line[:-1]:<50} - ERROR: {exc.args[0]}', _file=self.bad_log)
+                else:
+                    _write_log(_message=line[:-1], _file=self.good_log)
+
+
+log = LogParser(_log_file='registrations.txt')
+log.run()
